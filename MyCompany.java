2d0b39180c74eb7e34cf1ea458d1a59114bcf87c
rajpatwekar <112938076+rajpatwekar@users.noreply.github.com>
2022-09-15 13:46:43 +0530

    Add files via upload

---
+++ MyCompany.java
@@ -0,0 +1,16 @@
+class MyCompany 
+{
+static public void main(String args[])
+{ 
+
+{
+ int a=30000;
+ int x=11000;
+ int y=5000;
+ int z=a-x-y;
+System.out.print("salary of the third employee"+" "+z);
+}
+
+}
+
+}  
